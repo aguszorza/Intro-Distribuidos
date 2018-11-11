a66ea5ea71da9cef0cca811be8c5500837af1ae1
feldmatias <feldmatias@gmail.com>
2018-11-11 15:13:40 -0300

    calculate paths for packets, and update floww table

--- controller.py
+++ controller.py
@@ -14,6 +14,7 @@ from pox.lib.util import dpidToStr
 from pox.lib.recoco import Timer
 import time
 from pox.host_tracker import host_tracker
+import random
 
 # Create a logger for this component
 log = core.getLogger()
@@ -37,50 +38,111 @@ class MyController(EventMixin):
         packet = event.parsed
         dpid = event.connection.dpid
 
+        eth_packet = packet.find(pkt.ethernet)
+        ip_packet = packet.find(pkt.ipv4)
+        arp_packet = packet.find(pkt.arp)
+        icmp_packet = packet.find(pkt.icmp)
+        tcp_packet = packet.find(pkt.tcp)
+        udp_packet = packet.find(pkt.udp)
+
+        if icmp_packet is None and tcp_packet is None and udp_packet is None and arp_packet is None:
+            return
+
         def flood():
             #Flood incoming packet if dst is not known yet
             #Do not update flow table
-            print "Flooding packet in switch: " + dpidToStr(event.connection.dpid) + " --- dst=" + str(packet.find(pkt.ethernet).dst)
+            if (arp_packet is None):
+                print "Flooding packet in switch: " + dpidToStr(event.connection.dpid) + " --- dst=" + str(eth_packet.dst) + '\n'
             msg = of.ofp_packet_out()
             msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
             msg.data = event.ofp
             msg.in_port = event.port
             event.connection.send(msg)
 
-        ip6_packet = packet.find(pkt.ipv6)
-        if ip6_packet is not None:
-            return
 
-        dst = core.host_tracker.getMacEntry(packet.find(pkt.ethernet).dst)
-        if dst is None:
+        dstEntry = core.host_tracker.getMacEntry(eth_packet.dst)
+        if dstEntry is None or arp_packet is not None:
             return flood()
 
-        print "Calculating packet path in switch: " + dpidToStr(event.connection.dpid) + " --- dst=" + str(packet.find(pkt.ethernet).dst)
-        #print dpidToStr(event.connection.dpid)
+        print "Calculating packet path in switch: " + dpidToStr(event.connection.dpid) + " --- dst=" + str(eth_packet.dst)
+
+        dst = dstEntry.dpid
+
+        if (dst == dpid):
+            port = dstEntry.port
+        else:
+
+            paths = [[neighbour] for neighbour in self.getNeighbours(dpid)]
+            dsts = self.getPathsToDst(paths, dst)
+            while not dsts:
+                oldPaths = paths
+                paths = []
+                for path in oldPaths:
+                    neighbours = self.getNeighbours(path[-1].dpid2)
+                    for neighbour in neighbours:
+                        paths.append(path + [neighbour])
+                dsts = self.getPathsToDst(paths, dst)
+            
+            #dsts has all possible minnimum paths to dst
+            dstPath = dsts[self.getHash(ip_packet, tcp_packet, udp_packet) % len(dsts)]
+            port = dstPath[0].port1
+
+
+
+        text = "Making rule for sending packet in switch: " + dpidToStr(event.connection.dpid) + '\n'
+        text += "Ethernet: " + str(eth_packet.src) + " -> " + str(eth_packet.dst) + '\n'
+
+        #update flow table
+        msg = of.ofp_flow_mod()
+        msg.match.dl_type = eth_packet.type
+        msg.match.nw_src = ip_packet.srcip
+        msg.match.nw_dst = ip_packet.dstip
+        msg.match.nw_proto = ip_packet.protocol
+        text += "IPv4: " + str(ip_packet.srcip) + " -> " + str(ip_packet.dstip) + '\n'
+        if tcp_packet is not None:
+            msg.match.tp_src = tcp_packet.srcport
+            msg.match.tp_dst = tcp_packet.dstport
+            text += "TCP: " + str(tcp_packet.srcport) + " -> " + str(tcp_packet.dstport) + '\n'
+        if udp_packet is not None:
+            msg.match.tp_src = udp_packet.srcport
+            msg.match.tp_dst = udp_packet.dstport
+            text += "UDP: " + str(udp_packet.srcport) + " -> " + str(udp_packet.dstport) + '\n'
+        msg.actions.append(of.ofp_action_output(port = port))
+        event.connection.send(msg)
+        print text
+
+        #send packet
+        msg = of.ofp_packet_out()
+        msg.actions.append(of.ofp_action_output(port = port))
+        msg.data = event.ofp
+        msg.in_port = event.port
+        event.connection.send(msg)
+
+
+
+    def getNeighbours(self, dpid):
+        neighbours = []
         for adjacency in core.openflow_discovery.adjacency:
             if adjacency.dpid1 == dpid:
-                pass
-        return
+                neighbours.append(adjacency)
+        return neighbours
 
-        eth_packet = packet.find(pkt.ethernet)
-        if eth_packet is not None:
-            src_mac = eth_packet.src
-            dst_mac = eth_packet.dst
-            print "ETHERNET: src=" + str(src_mac) + " dst=" + str(dst_mac)
+    def getPathsToDst(self, paths, dst):
+        dstPaths = []
+        for path in paths:
+            if path[-1].dpid2 == dst:
+                dstPaths.append(path)
+        return dstPaths
 
-        ip_packet = packet.find(pkt.ipv4)
-        if ip_packet is not None:
-            src_ip = ip_packet.srcip
-            dst_ip = ip_packet.dstip
-            print "IP: src=" + str(src_ip) + " dst=" + str(dst_ip)
+    def getHash(self, ip_packet, tcp_packet, udp_packet):
+        number = abs(hash((ip_packet.srcip, ip_packet.dstip, ip_packet.protocol)))
 
-        tcp_packet = packet.find(pkt.tcp)
         if tcp_packet is not None:
-            src_port = tcp_packet.srcport
-            dst_port = tcp_packet.dstport
-            print "TCP: src=" + str(src_port) + " dst=" + str(dst_port)
+            number += abs(hash((tcp_packet.srcport, tcp_packet.dstport)))
+        if udp_packet is not None:
+            number += abs(hash((udp_packet.srcport, udp_packet.dstport)))
 
-
+        return number
 
 
 def launch ():
